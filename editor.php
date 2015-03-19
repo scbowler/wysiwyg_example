f009265b9919919052ce378fa61c2abe12d1e41c
dpaschal <daniel.paschal@gmail.com>
2015-03-19 11:41:05 -0700

    initial commit

---
+++ editor.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>WYSIWYG editor</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html>
+<head>
+<?php 
+$intro_message = 'This is our editor area';
+?>
+<script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
+<script>tinymce.init({selector:'#detail_textarea'});</script>
+</head>
+<body>
+        <textarea name="editor" id='detail_textarea'><?php echo "$intro_message";?></textarea>
+</body>
+</html>
+
+
+
+</body>
+</html>

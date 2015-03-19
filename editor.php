8ed45a1ebe1585e97d113c622a2837ce47c42bf8
dpaschal <daniel.paschal@gmail.com>
2015-03-19 14:25:08 -0700

    fixed errant tags

--- editor.php
+++ editor.php
@@ -5,16 +5,11 @@
     <title>WYSIWYG editor</title>
 </head>
 <body>
-<!DOCTYPE html>
-<html>
-<head>
 <?php 
 $intro_message = 'This is our editor area';
 ?>
 <script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
 <script>tinymce.init({selector:'#detail_textarea'});</script>
-</head>
-<body>
         <textarea name="editor" id='detail_textarea'><?php echo "$intro_message";?></textarea>
 </body>
 </html>
